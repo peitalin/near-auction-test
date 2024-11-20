05db6805d8219d940ea1e14f63e8d0d80c8cedcd
Peita <n6378056@gmail.com>
2024-11-20 15:08:56 +0700

    get auction example working

--- src/lib.rs
+++ src/lib.rs
@@ -1,137 +1,158 @@
-use near_sdk::{
-    env,
-    log,
-    near,
-    require,
-    AccountId,
-    NearToken,
-    PanicOnDefault,
-    Promise,
-    json_types::U64
-};
+// Find all our documentation at https://docs.near.org
+use near_sdk::json_types::{U128, U64};
+use near_sdk::{env, near, require, AccountId, Gas, NearToken, PanicOnDefault};
 
+pub mod ext;
+pub use crate::ext::*;
 
 #[near(serializers = [json, borsh])]
 #[derive(Clone)]
 pub struct Bid {
     pub bidder: AccountId,
-    pub bid: NearToken
+    pub bid: U128,
 }
 
-#[near(contract_state)]
+pub type TokenId = String;
+
+#[near(contract_state, serializers = [json, borsh])]
 #[derive(PanicOnDefault)]
 pub struct Contract {
     highest_bid: Bid,
     auction_end_time: U64,
     auctioneer: AccountId,
     claimed: bool,
+    ft_contract: AccountId,
+    nft_contract: AccountId,
+    token_id: TokenId,
 }
 
 #[near]
 impl Contract {
-
     #[init]
-    #[private]
-    pub fn init(end_time: U64, auctioneer: AccountId) -> Self {
+    #[private] // only callable by the contract's account
+    pub fn init(
+        end_time: U64,
+        auctioneer: AccountId,
+        ft_contract: AccountId,
+        nft_contract: AccountId,
+        token_id: TokenId,
+        starting_price: U128,
+    ) -> Self {
         Self {
             highest_bid: Bid {
                 bidder: env::current_account_id(),
-                bid: NearToken::from_yoctonear(1),
+                bid: starting_price,
             },
             auction_end_time: end_time,
-            auctioneer: auctioneer,
+            auctioneer,
             claimed: false,
+            ft_contract,
+            nft_contract,
+            token_id,
         }
     }
 
-    #[payable]
-    pub fn bid(&mut self) -> Promise {
-        require!(env::block_timestamp() < self.auction_end_time.into(), "Auction had ended");
+    // Users bid by transferring FT tokens
+    pub fn ft_on_transfer(&mut self, sender_id: AccountId, amount: U128, msg: String) -> U128 {
+        require!(
+            env::block_timestamp() < self.auction_end_time.into(),
+            "Auction has ended"
+        );
 
-        let bid = env::attached_deposit();
-        let bidder = env::predecessor_account_id();
+        let ft = env::predecessor_account_id();
+        require!(ft == self.ft_contract, "The token is not supported");
 
+        // Last bid
         let Bid {
             bidder: last_bidder,
-            bid: last_bid
+            bid: last_bid,
         } = self.highest_bid.clone();
 
-        require!(bid > last_bid, "bid must exceed highest bid");
+        // Check if the deposit is higher than the current bid
+        require!(amount > last_bid, "You must place a higher bid");
 
-        self.highest_bid = Bid { bid, bidder };
+        // Update the highest bid
+        self.highest_bid = Bid {
+            bidder: sender_id,
+            bid: amount,
+        };
 
-        // transfer tokens back to previous high bidder
-        Promise::new(last_bidder).transfer(last_bid)
+        // Transfer FTs back to the last bidder
+        ft_contract::ext(self.ft_contract.clone())
+            .with_attached_deposit(NearToken::from_yoctonear(1))
+            .with_static_gas(Gas::from_tgas(30))
+            .ft_transfer(last_bidder, last_bid);
+
+        U128(0)
     }
 
-    pub fn claim(&mut self) -> Promise {
-        require!(env::block_timestamp() > self.auction_end_time.into(), "auction not ended");
-        require!(!self.claimed, "auction already claimed");
+    pub fn claim(&mut self) {
+        require!(
+            env::block_timestamp() > self.auction_end_time.into(),
+            "Auction has not ended yet"
+        );
+
+        require!(!self.claimed, "Auction has been claimed");
+
         self.claimed = true;
-        // Transfer tokens to auctioneer
-        Promise::new(self.auctioneer.clone()).transfer(self.highest_bid.bid)
+
+        // Transfer FTs to the auctioneer
+        ft_contract::ext(self.ft_contract.clone())
+            .with_attached_deposit(NearToken::from_yoctonear(1))
+            .with_static_gas(Gas::from_tgas(30))
+            .ft_transfer(self.auctioneer.clone(), self.highest_bid.bid);
+
+        // Transfer the NFT to the highest bidder
+        nft_contract::ext(self.nft_contract.clone())
+            .with_static_gas(Gas::from_tgas(30))
+            .with_attached_deposit(NearToken::from_yoctonear(1))
+            .nft_transfer(self.highest_bid.bidder.clone(), self.token_id.clone());
     }
 
-    pub fn get_highest_bid(&self) -> &Bid {
-        &self.highest_bid
+    pub fn get_highest_bid(&self) -> Bid {
+        self.highest_bid.clone()
     }
 
-    pub fn get_auction_end_time(&self) -> &U64 {
-        &self.auction_end_time
+    pub fn get_auction_end_time(&self) -> U64 {
+        self.auction_end_time
     }
 
+    pub fn get_auction_info(&self) -> &Contract {
+        self
+    }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-// // Define the contract structure
-// #[near(contract_state)]
-// pub struct Contract {
-//     greeting: String,
-// }
+    #[test]
+    fn init_contract() {
+        let end_time: U64 = U64::from(1000);
+        let alice: AccountId = "alice.near".parse().unwrap();
+        let ft_contract: AccountId = "ft.near".parse().unwrap();
+        let nft_contract: AccountId = "nft.near".parse().unwrap();
+        let token_id: TokenId = "1".to_string();
+        let starting_price: U128 = U128(100);
+        let contract = Contract::init(
+            end_time.clone(),
+            alice.clone(),
+            ft_contract.clone(),
+            nft_contract.clone(),
+            token_id.clone(),
+            starting_price.clone(),
+        );
 
-// // Define the default, which automatically initializes the contract
-// impl Default for Contract {
-//     fn default() -> Self {
-//         Self {
-//             greeting: "Hello".to_string(),
-//         }
-//     }
-// }
+        let default_bid = contract.get_highest_bid();
+        assert_eq!(default_bid.bidder, env::current_account_id());
+        assert_eq!(default_bid.bid, starting_price);
 
-// // Implement the contract structure
-// #[near]
-// impl Contract {
-//     // Public method - returns the greeting saved, defaulting to DEFAULT_GREETING
-//     pub fn get_greeting(&self) -> String {
-//         self.greeting.clone()
-//     }
-
-//     // Public method - accepts a greeting, such as "howdy", and records it
-//     pub fn set_greeting(&mut self, greeting: String) {
-//         log!("Saving greeting: {greeting}");
-//         self.greeting = greeting;
-//     }
-// }
-
-// /*
-//  * The rest of this file holds the inline tests for the code above
-//  * Learn more about Rust tests: https://doc.rust-lang.org/book/ch11-01-writing-tests.html
-//  */
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn get_default_greeting() {
-//         let contract = Contract::default();
-//         // this test did not call set_greeting so should return the default "Hello" greeting
-//         assert_eq!(contract.get_greeting(), "Hello");
-//     }
-
-//     #[test]
-//     fn set_then_get_greeting() {
-//         let mut contract = Contract::default();
-//         contract.set_greeting("howdy".to_string());
-//         assert_eq!(contract.get_greeting(), "howdy");
-//     }
-// }
+        let auction_info = contract.get_auction_info();
+        assert_eq!(auction_info.auction_end_time, end_time);
+        assert_eq!(auction_info.auctioneer, alice);
+        assert_eq!(auction_info.ft_contract, ft_contract);
+        assert_eq!(auction_info.nft_contract, nft_contract);
+        assert_eq!(auction_info.token_id, token_id);
+        assert_eq!(auction_info.claimed, false);
+    }
+}
